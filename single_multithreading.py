f75ee99d1e87373c5a015f775ee236dfb81bb0d1
chaos <497386856@qq.com>
2016-11-29 00:09:07 +0800

    完成多线程

--- single_multithreading.py
+++ single_multithreading.py
@@ -1,6 +1,7 @@
 __author__='RealmL'
 
 import random
+from _thread import start_new_thread
 
 def metrix_init(m1:list,m2:list,res:list,x:int,y:int):
 
@@ -25,19 +26,41 @@ def metrix_init(m1:list,m2:list,res:list,x:int,y:int):
     return m1,m2,res
 
 
+def multiTwoLine(l1: list, l2: list, res: list, i: int):
+    #每个线程需要执行的函数
+    for j in range(len(l2[0])):
+        a = l1[i]
+        b = [row[j] for row in l2]
+        sum = 0
+        for k in range(len(m2)):
+            sum += a[k] * b[k]
+        res[i][j] = sum
+
+
 if __name__ =='__main__':
-    m1,m2,res=metrix_init([],[],[],0,100)
-    print(m1)
-    print(m2)
+    m1,m2,res=metrix_init([],[],[],0,1000)
+    print(len(m1))
+    print(len(m2))
 
+    #单线程
+    # for i in range(len(m1)):
+    #     for j in range(len(m2[0])):
+    #         a=m1[i]
+    #         b=[row[j] for row in m2]
+    #         sum=0
+    #         for k in range(len(m2)):
+    #             sum+=a[k]*b[k]
+    #         res[i][j]=sum
+    #
+    # print(len(res))
+
+    #多线程
     for i in range(len(m1)):
-        for j in range(len(m2[0])):
-            a=m1[i]
-            b=[row[j] for row in m2]
-            sum=0
-            for k in range(len(m2)):
-                sum+=a[k]*b[k]
-            res[i][j]=sum
+        start_new_thread(multiTwoLine,(m1,m2,res,i))
 
-    print(res)
+    print(len(res))
 
+
+    # multiTwoLine(m1,m2,res,0)
+
+    print(res[0][0])
